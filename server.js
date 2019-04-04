d012023625730df62d900edea27cf8a9c454444b
rasika.r.ekhalikar@tsgforce.com <rasika.r.ekhalikar@tsgforce.com>
2019-04-04 13:04:40 +0000

    changed port

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use("/", express.static(__dirname + '/www'));
 
-app.listen('4000');
-console.log('working on 4000');
+// app.listen('4000');
+// console.log('working on 4000');
 
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
