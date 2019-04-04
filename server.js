23e18378e84ba9700c4358d99830f01fbda0b9f2
rasika.r.ekhalikar@tsgforce.com <rasika.r.ekhalikar@tsgforce.com>
2019-04-04 12:59:47 +0000

    edited structure

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/www'));
+app.use("/", express.static(__dirname + '/www'));
 
-app.listen('3000');
-console.log('working on 3000');
+app.listen('4000');
+console.log('working on 4000');
 
